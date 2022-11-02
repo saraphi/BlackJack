7ecc53055665882944c8502aa810f7075cdabd31
Sara González Ramírez <saragonza.lez0608@gmail.com>
2022-11-02 11:39:22 +0000

    Blackjack profe clase 1

---
+++ src/model/Hand.java
@@ -0,0 +1,7 @@
+package model;
+
+public interface Hand {
+    
+    public void add();
+    public int value();
+}
---
+++ test/test/BlackjackTest.java
@@ -0,0 +1,10 @@
+package test;
+
+import model.*;
+import org.junit.jupiter.api.Test;
+
+public class BlackjackTest {
+    
+    @Test
+    public void test_hand_value_with_one_card() {}
+}
